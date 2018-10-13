5ac9288704743449d40630eed485b4f6d278f3a5
Taki Uddin <taki.uddin@plusequalsto.com>
2018-10-14 02:39:45 +0600

    Bottomsheet implemented

--- app/src/main/java/plusequalsto/com/radio/MainActivity.java
+++ app/src/main/java/plusequalsto/com/radio/MainActivity.java
@@ -3,60 +3,68 @@ package plusequalsto.com.radio;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private BottomSheetBehavior mBottomSheetBehavior;
-    ConstraintLayout tapactionlayout;
+    LinearLayout tapactionlayout;
+    ConstraintLayout lol;
     ImageView arrowUp, playpauseIcon;
     View bottomSheet;
+    RelativeLayout.LayoutParams  tap_action_layout;
+
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tapactionlayout = (LinearLayout) findViewById(R.id.tap_action_layout);
+        lol = (ConstraintLayout) findViewById(R.id.lol);
+        ViewTreeObserver vto = lol.getViewTreeObserver();
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            public boolean onPreDraw() {
+                lol.getViewTreeObserver().removeOnPreDrawListener(this);
+                int finalHeight = lol.getMeasuredHeight();
+                int finalWidth = lol.getMeasuredWidth();
 
-        bottomSheet = findViewById(R.id.bottom_sheet);
-        tapactionlayout = (ConstraintLayout) findViewById(R.id.tap_action_layout);
+                Toast.makeText(MainActivity.this, finalHeight + " " + finalWidth, LENGTH_LONG ).show();
+                return true;
+            }
+        });
+
         arrowUp = (ImageView) findViewById(R.id.arrowUp);
-        playpauseIcon = (ImageView) findViewById(R.id.playpauseIcon);
-        final ConstraintLayout.LayoutParams  tap_action_layout = (ConstraintLayout.LayoutParams) playpauseIcon.getLayoutParams();
-//        View view_instance = (View)findViewById(R.id.playpauseIcon);
+        playpauseIcon = (ImageView) findViewById(R.id.playpause);
+        bottomSheet = findViewById(R.id.bottom_sheet);
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        mBottomSheetBehavior.setPeekHeight(250);
+        mBottomSheetBehavior.setPeekHeight(158);
         mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-        arrowUp.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+
         mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
                 if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
                     tapactionlayout.setVisibility(View.VISIBLE);
-                    arrowUp.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
-                    playpauseIcon.requestLayout();
-                    playpauseIcon.getLayoutParams().height  = 200;
-                    playpauseIcon.getLayoutParams().width  = 200;
-                    tap_action_layout.setMargins(83, 8, 8, 0);
-                    playpauseIcon.setScaleType(ImageView.ScaleType.FIT_XY);
                 }
 
                 if (newState == BottomSheetBehavior.STATE_EXPANDED) {
                     tapactionlayout.setVisibility(View.VISIBLE);
-                    playpauseIcon.requestLayout();
-                    playpauseIcon.getLayoutParams().height  = 500;
-                    playpauseIcon.getLayoutParams().width  = 500;
-                    tap_action_layout.setMargins(20, 500, 20, 20);
-                    playpauseIcon.setScaleType(ImageView.ScaleType.FIT_XY);
                 }
 
                 if (newState == BottomSheetBehavior.STATE_DRAGGING) {
                     tapactionlayout.setVisibility(View.VISIBLE);
-                    arrowUp.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
                 }
             }
 
@@ -73,20 +81,13 @@ public class MainActivity extends AppCompatActivity {
                 {
                     mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                     arrowUp.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
-                    playpauseIcon.getLayoutParams().height  = 500;
-                    playpauseIcon.getLayoutParams().width  = 500;
-                    tap_action_layout.setMargins(20, 500, 20, 20);
-                    playpauseIcon.setScaleType(ImageView.ScaleType.FIT_XY);
+                    playpauseIcon.setVisibility(View.GONE);
                 } else {
                     mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
                     arrowUp.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
-                    playpauseIcon.getLayoutParams().height  = 200;
-                    playpauseIcon.getLayoutParams().width  = 200;
-                    tap_action_layout.setMargins(83, 8, 8, 0);
-                    playpauseIcon.setScaleType(ImageView.ScaleType.FIT_XY);
+                    playpauseIcon.setVisibility(View.VISIBLE);
                 }
             }
         });
-
     }
 }
